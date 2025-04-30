4a25bfe76b6310379bab5ad8330d206d23b3480c
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-04-30 19:46:23 -0300

    Api trabalho

--- API/trabalho_api_01.py
+++ API/trabalho_api_01.py
@@ -3,12 +3,12 @@ from datetime import datetime
 import streamlit as st
 
 st.title('Descubra o clima de sua cidade')
-st.markdown('---')
+st.markdown('-----')
 
 # Pega o nome da cidade
 cidade = st.text_input('Digite sua cidade:')
 
-if cidade:  # Só executa se o usuário digitou algo
+if cidade != '':  # Só executa se o usuário digitou algo
     try:
         # Obtém o código da cidade
         url_cidade = f'https://brasilapi.com.br/api/cptec/v1/cidade/{cidade}' 
@@ -30,14 +30,15 @@ if cidade:  # Só executa se o usuário digitou algo
         max_temp = clima_cidade[0]['max']
 
         # Exibe os resultados
-        st.markdown('---')
+        st.markdown('-----')
         st.subheader(f'Previsão do tempo para {cidade}')
         st.write(f'**Data:** {data_}')
         st.write(f'**Condição:** {descricao}')
         st.write(f'**Temperatura mínima:** {min_temp}°C')
         st.write(f'**Temperatura máxima:** {max_temp}°C')
-        st.markdown('---')
+        st.markdown('-----')
 
     except Exception as e:
         st.error('Ocorreu um erro ao buscar a previsão do tempo. Verifique o nome da cidade para tentar de novo.')
-        st.error(f'Detalhes do erro: {str(e)}')
+        
+st.time_input
--- orientacao_objs/metodo_os.py
+++ orientacao_objs/metodo_os.py
@@ -7,11 +7,18 @@ class Gerenciando_Diretório:
     def criar_pastas(self,url, nome_pasta):
         juncao = os.path.join(url, nome_pasta)
         os.makedirs(juncao)
+    
+    def editar_nomes(self,url,novo_nome):
+        os.rename(url,novo_nome)
+    
+    def listar(self,diretorio):
+        os.remove() 
         
 
 diretorio = Gerenciando_Diretório()
 
-diretorio.criar_pastas("C:\\Users\\999532\\Documents\\Curso_Tecnico\\orientacao_objs\\teste01\\teste02", "teste03")
+# diretorio.criar_pastas("C:\\Users\\999532\\Documents\\Curso_Tecnico\\orientacao_objs\\teste01\\teste02", "teste03")
+# diretorio.editar_nomes("C:\\Users\\999532\\Documents\\Curso_Tecnico\\orientacao_objs\\teste01", "C:\\Users\\999532\\Documents\\Curso_Tecnico\\orientacao_objs\\novo_nome01")
 
 
 
