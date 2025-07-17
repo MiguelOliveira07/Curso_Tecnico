d77e68af5cf8e0e61423e52ad36d7a2c448911d4
MiguelOliveira07 <137643185+MiguelOliveira07@users.noreply.github.com>
2025-07-16 21:35:32 -0300

    Codigo espellho de input (Entry)

--- Python/Tkinter/mirror.py
+++ Python/Tkinter/mirror.py
@@ -3,19 +3,30 @@ import tkinter as tk
 app = tk.Tk()
 
 tk.Label(master=app, 
-        text='Nome',
+    text='Nome',
 ).pack(
     padx=5,
     pady=5
 )
 
 nome_var = tk.StringVar()
+
 nome_entry = tk.Entry(
     master=app,
     textvariable=nome_var
 ).pack(
     padx=5,
     pady=5
+)
+
+nome_label = tk.Label(
+    master=app,
+    textvariable=nome_var,
+    font=('Arial', 20, 'normal')
+).pack(
+    padx=5,
+    pady=5,
+    anchor=tk.W
 )
 
 app.mainloop()
