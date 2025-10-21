f9b79c3cb6df5d62ba1ef2ec244dcd985dad249b
Miguel <miguelalberto6110@gmail.com>
2025-10-21 20:25:54 -0300

    lampada

---
+++ Básico/js_inicio/main.js
@@ -0,0 +1,19 @@
+// document.addEventListener('DOMContentLoaded', ()=>{} )
+
+// function name(params){
+//     let num = 0
+//     return num
+// }
+
+let luz = document.getElementById('luz')
+
+// document.addEventListener('DOMContentLoaded', () =>{
+    function ligar() {
+        luz.src="imgs\\ligada.jpg"
+    }
+
+    function apagar() {
+        luz.src="imgs\\apagada.png"
+    }
+
+// })
